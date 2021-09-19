f28e4a58eb76ef39ff926936f4f4dddba9b3cb26
Nataliia Kotelnitska <83007830+Natali-Kotelnitska@users.noreply.github.com>
2021-09-19 23:44:20 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -4,3 +4,30 @@
 // Створи змінну counterValue в якій буде зберігається поточне значення   лічильника.
 // Створи функції increment і decrement для збільшення і зменшення значення   лічильника.
 // Додай слухачі кліків на кнопки, виклики функцій та оновлення інтерфейсу
+
+let counterValue = 0;
+
+const incrementBtnEl = document.querySelector(
+  '#counter button[data-action="increment"]',
+)
+const decrementBtnEl = document.querySelector(
+  '#counter button[data-action="decrement"]',
+)
+
+function setCounterValue() {
+  document.getElementById('value').textContent = counterValue
+}
+
+function increment() {
+  counterValue += 1
+  setCounterValue()
+}
+
+function decrement() {
+  counterValue -= 1
+  setCounterValue()
+}
+
+incrementBtnEl.addEventListener('click', increment)
+decrementBtnEl.addEventListener('click', decrement)
+
