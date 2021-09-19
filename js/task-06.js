84b737ec470ac173b628fb4a4be574133d1641a3
Nataliia Kotelnitska <83007830+Natali-Kotelnitska@users.noreply.github.com>
2021-09-19 23:22:22 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,3 +4,20 @@
 // Скільки символів має бути в інпут, вказується в його атрибуті data-length.
 // Якщо введена відповідна кількість, то border інпут стає зеленим,   якщо неправильне - червоним.
 // Для додавання стилів, використовуй CSS-класи valid і invalid.
+
+const inputValidationEl = document.querySelector("#validation-input");
+
+const inputLength = Number(inputValidationEl.dataset.length);
+// console.log(typeof inputLength);
+
+inputValidationEl.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event) {
+    if (event.currentTarget.value.length === inputLength) {
+        inputValidationEl.classList.add('valid')
+        inputValidationEl.classList.remove('invalid')
+    } else {
+        inputValidationEl.classList.remove('valid')
+        inputValidationEl.classList.add('invalid')
+    }      
+};
