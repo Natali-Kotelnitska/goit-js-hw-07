8e31c7d9666dd6348bb959952c818d53f1e10927
Nataliia Kotelnitska <83007830+Natali-Kotelnitska@users.noreply.github.com>
2021-09-19 23:54:38 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,3 +2,13 @@
 // input#font - size - control(подія input) 
 // і змінює інлайн - стиль span#text оновлюючи властивість font - size.
 // В результаті при перетягуванні повзунка змінюватиметься розмір тексту.
+
+const rangeControlEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector("#text");
+
+rangeControlEl.addEventListener('change', onTextSizeChange);
+
+function onTextSizeChange(event) {
+    textEl.style.fontsize = event.currentTarget.value + 'px';
+
+};
