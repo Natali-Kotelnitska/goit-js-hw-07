50d56f3136d1b2deb2a803f49f02e8d4f498d334
Nataliia Kotelnitska <83007830+Natali-Kotelnitska@users.noreply.github.com>
2021-09-19 22:36:27 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,19 @@
 // Напиши скрипт який, при наборі тексту в інпут input#name - input(подія input),
 //     підставляє його поточне значення в span#name - output.якщо інпут порожній,
 //     в спані повинен відображатися рядок 'незнайомець'.
+const refs = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output'),
+};
+
+
+refs.input.addEventListener('input', onOutputChange);
+
+function onOutputChange(event) {
+   
+    if (event.currentTarget.value) {
+        refs.output.textContent = event.currentTarget.value
+    } else {
+        refs.output.textContent = "незнакомец";
+    }
+};
